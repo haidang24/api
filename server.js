1641d06dda8f75796641974caa8b3dcce3764aca
Hải Đăng <117503840+haidang24@users.noreply.github.com>
2023-04-20 23:06:25 +0700

    update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,16 +10,22 @@ app.use(express.json());
 
 const data=[
   {
-    name: "Lối nhỏ",
-    author: "Đen Vâu"
+    name: "Sóng Gió",
+    author: "Jack",
+    id: 1,
+    link: "https://api-0vri.onrender.com/public/sóng gió.mp3"
   },
     {
     name: "Trốn tìm",
-    author: "Đen Vâu"
+    author: "Đen Vâu",
+    id: 2,
+    link: "https://api-0vri.onrender.com/public/sóng gió.mp3"
   },
   {
     name: "Đi về nhà",
-    author: "Đen Vâu"
+    author: "Đen Vâu",
+    id: 3,
+    link: "https://api-0vri.onrender.com/public/sóng gió.mp3"
   },
   
   
@@ -27,6 +35,13 @@ const data=[
 app.get('/api/music',(req,res)=>{
   res.json(data)
 })
+
+app.get('/public/:filename', (req, res) => {
+  const fileName = req.params.filename;
+  const filePath = path.join(__dirname, 'public', fileName);
+  res.sendFile(filePath);
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
